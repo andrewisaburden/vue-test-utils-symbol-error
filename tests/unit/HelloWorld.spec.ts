cd0a05f4885cd5a58e0a2273c7edb99b9406497a
Andrew Burden <andrew@webscope.co.nz>
2018-07-19 10:00:11 +1200

    Added vue-js-modal and reproduced bug.

--- tests/unit/HelloWorld.spec.ts
+++ tests/unit/HelloWorld.spec.ts
@@ -1,10 +1,15 @@
-import { shallowMount } from '@vue/test-utils';
+import { mount, createLocalVue } from '@vue/test-utils';
 import HelloWorld from '@/components/HelloWorld.vue';
+import VModal from 'vue-js-modal';
+
+const localVue = createLocalVue();
+localVue.use(VModal);
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = mount(HelloWorld, {
+      localVue,
       propsData: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
